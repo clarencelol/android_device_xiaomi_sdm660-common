omnirom/android_device_nokia_sdm660-common

sdm660-common: Checkout config.fs to LA.UM.7.2.r1-07200-sdm660.0 from https://source.codeaurora.org/quic/la/device/qcom/common/
Change-Id: Ieb12614c220619de9e1c478b24a6a218114f3c15

config.fs
@@ -1,5 +1,20 @@
-[AID_QTI_DIAG]
+[AID_VENDOR_QTI_DIAG]
 value:2901
+
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
 
 [vendor/bin/wcnss_filter]
 mode: 0755
@@ -13,13 +28,13 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -49,7 +64,19 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/imsdatadaemon]
+[vendor/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pd-mapper]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -61,22 +88,10 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/cnd]
+[system/vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
-[vendor/bin/cnd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
-
-[system/vendor/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
@@ -85,11 +100,35 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/slim_daemon]
+[system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user:  AID_GPS
-group: AID_GPS
+user: AID_SYSTEM
+group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/slim_daemon]
 mode: 0755
@@ -97,9 +136,51 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 

clarencelol/android_device_xiaomi_sdm660-common

config.fs
@@ -1,6 +1,3 @@
-[AID_VENDOR_QCOM_DIAG]
-value:2950
-
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
@@ -8,13 +5,19 @@ value:2901
 value:2902
 
 [AID_VENDOR_RFS]
-value:2951
+value:2903
 
 [AID_VENDOR_RFS_SHARED]
-value:2952
+value:2904
 
 [AID_VENDOR_ADPL_ODL]
 value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
+[AID_VENDOR_RFS]
+value:2951
 
 [vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
@@ -118,10 +121,10 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[vendor/bin/sensors.qti]
+[vendor/bin/slim_daemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user:  AID_GPS
+group: AID_GPS
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/sensors.qti]
@@ -136,10 +139,28 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/slim_daemon]
+[vendor/bin/loc_launcher]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
 [vendor/bin/loc_launcher]