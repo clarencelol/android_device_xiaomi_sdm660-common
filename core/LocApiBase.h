Aldair402/platform_vendor_qcom_opensource_gps-legacy

GPS HAL: send platform power state event to modem
Send platform power state event, suspend/resume/shutdown to modem

Change-Id: I19960f5fb4935de9e4f5513abd95aed6241a2fd9
CRs-fixed: 2527622

core/LocApiBase.h
@@ -298,6 +298,7 @@ public:
                                                  uint32_t energyBudget);
     virtual LocationError setPositionAssistedClockEstimatorMode(bool enabled);
     virtual LocationError getGnssEnergyConsumed();
+    virtual void updatePowerState(PowerStateType powerState);
 };
 
 typedef LocApiBase* (getLocApi_t)(LOC_API_ADAPTER_EVENT_MASK_T exMask,

gnss/GnssAdapter.h
@@ -181,6 +181,7 @@ class GnssAdapter : public LocAdapterBase {
     XtraSystemStatusObserver mXtraObserver;
     LocationSystemInfo mLocSystemInfo;
     std::vector<GnssSvIdSource> mBlacklistedSvIds;
+    PowerStateType mPowerState;
 
     /* === Misc ===================================================================== */
     BlockCPIInfo mBlockCPIInfo;
@@ -230,6 +231,8 @@ public:
     void stopClientSessions(LocationAPI* client);
     LocationCallbacks getClientCallbacks(LocationAPI* client);
     LocationCapabilitiesMask getCapabilities();
+    inline PowerStateType getPowerState() { return mPowerState; }
+
     void broadcastCapabilities(LocationCapabilitiesMask);
     void setSuplHostServer(const char* server, int port, LocServerType type);
 
@@ -398,6 +401,7 @@ public:
     void invokeGnssEnergyConsumedCallback(uint64_t energyConsumedSinceFirstBoot);
     void saveGnssEnergyConsumedCallback(GnssEnergyConsumedCallback energyConsumedCb);
     void reportLocationSystemInfo(const LocationSystemInfo & locationSystemInfo);
+    void updatePowerState(PowerStateType powerState);
 
     /*======== GNSSDEBUG ================================================================*/
     bool getDebugReport(GnssDebugReport& report);
@@ -437,6 +441,8 @@ public:
     void injectTimeCommand(int64_t time, int64_t timeReference, int32_t uncertainty);
     void blockCPICommand(double latitude, double longitude, float accuracy,
                          int blockDurationMsec, double latLonDiffThreshold);
+
+    void updatePowerStateCommand(PowerStateType powerState);
 };
 
 #endif //GNSS_ADAPTER_H

location/location_interface.h
@@ -84,6 +84,7 @@ struct GnssInterface {
     void (*getGnssEnergyConsumed)(GnssEnergyConsumedCallback energyConsumedCb);
     void (*injectLocationExt)(const GnssLocationInfoNotification &locationInfo);
     void (*updateBatteryStatus)(bool charging);
+    void (*updatePowerState)(PowerStateType powerState);
 };
 
 struct FlpInterface {

clarencelol/android_device_xiaomi_sdm660-common

core/LocApiBase.h
@@ -312,6 +312,7 @@ public:
     void updateEvtMask();
     void updateNmeaMask(uint32_t mask);
 
+    virtual void updateSystemPowerState(PowerStateType systemPowerState);
 };
 
 typedef LocApiBase* (getLocApi_t)(LOC_API_ADAPTER_EVENT_MASK_T exMask,

gnss/GnssAdapter.h
@@ -187,6 +187,7 @@ class GnssAdapter : public LocAdapterBase {
     XtraSystemStatusObserver mXtraObserver;
     LocationSystemInfo mLocSystemInfo;
     std::vector<GnssSvIdSource> mBlacklistedSvIds;
+    PowerStateType mSystemPowerState;
 
     /* === Misc ===================================================================== */
     BlockCPIInfo mBlockCPIInfo;
@@ -413,6 +414,8 @@ public:
         return false;
     }
 
+    void updateSystemPowerState(PowerStateType systemPowerState);
+
     /*======== GNSSDEBUG ================================================================*/
     bool getDebugReport(GnssDebugReport& report);
     /* get AGC information from system status and fill it */
@@ -459,11 +462,14 @@ public:
     void reportPowerStateIfChanged();
     void savePowerStateCallback(powerStateCallback powerStateCb){ mPowerStateCb = powerStateCb; }
     bool getPowerState() { return mPowerOn; }
+    inline PowerStateType getSystemPowerState() { return mSystemPowerState; }
+
     void setAllowFlpNetworkFixes(uint32_t allow) { mAllowFlpNetworkFixes = allow; }
     uint32_t getAllowFlpNetworkFixes() { return mAllowFlpNetworkFixes; }
     void setSuplHostServer(const char* server, int port, LocServerType type);
     void notifyClientOfCachedLocationSystemInfo(LocationAPI* client,
                                                 const LocationCallbacks& callbacks);
+    void updateSystemPowerStateCommand(PowerStateType systemPowerState);
 };
 
 #endif //GNSS_ADAPTER_H

location/location_interface.h
@@ -88,6 +88,7 @@ struct GnssInterface {
     void (*getPowerStateChanges)(void* powerStateCb);
     void (*injectLocationExt)(const GnssLocationInfoNotification &locationInfo);
     void (*updateBatteryStatus)(bool charging);
+    void (*updateSystemPowerState)(PowerStateType systemPowerState);
 };
 
 struct BatchingInterface {