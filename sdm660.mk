93876fc3feb7d70483304091740858b091a62f39
clarencelol <clarencekuiek@icloud.com>
2022-05-10 15:05:13 +0800

    sdm660-common: Downgrade blobs to LA.UM.8.2.r1-07400-sdm660.0
    * In my testing Q blobs with Q Audio HAL have better results
    * Switched to prebuilt audio because it has fixed low audio record
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- sdm660.mk
+++ sdm660.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     audio.a2dp.default \
-    audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
     audio.usbv2.default \
@@ -91,7 +90,6 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libtinycompress \
     libvolumelistener \
     tinymix
 
