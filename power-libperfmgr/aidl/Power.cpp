7c066f0ab746c60f9cedfc8a5817f2bb7a61941c
clarencelol <clarencekuiek@icloud.com>
2022-05-16 18:16:33 +0800

    sdm660-common: power-libperfmgr: Nuked battery saver thru powerhal
    * Will switch to kprofile if needed
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- power-libperfmgr/aidl/Power.cpp
+++ power-libperfmgr/aidl/Power.cpp
@@ -56,7 +56,6 @@ Power::Power(std::shared_ptr<HintManager> hm)
     : mHintManager(hm),
       mInteractionHandler(nullptr),
       mSustainedPerfModeOn(false),
-      mBatterySaverOn(false),
       mAdpfRateNs(
               ::android::base::GetIntProperty(kPowerHalAdpfRateProp, kPowerHalAdpfRateDefault)) {
     mInteractionHandler = std::make_unique<InteractionHandler>(mHintManager);
@@ -87,12 +86,6 @@ Power::Power(std::shared_ptr<HintManager> hm)
     LOG(INFO) << "PowerHAL ready to take hints, Adpf update rate: " << mAdpfRateNs;
 }
 
-void endAllHints(std::shared_ptr<HintManager> mHintManager) {
-    for (std::string hint: mHintManager->GetHints()) {
-        mHintManager->EndHint(hint);
-    }
-}
-
 ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
     LOG(DEBUG) << "Power setMode: " << toString(type) << " to: " << enabled;
     ATRACE_INT(toString(type).c_str(), enabled);
@@ -104,24 +97,17 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
             [[fallthrough]];
             }
         case Mode::LOW_POWER:
-            if (enabled) {
-                endAllHints(mHintManager);
-                mHintManager->DoHint("LOW_POWER");
-            } else {
-                mHintManager->EndHint("LOW_POWER");
-            }
-            mBatterySaverOn = enabled;
             break;
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
-                endAllHints(mHintManager);
                 mHintManager->DoHint("SUSTAINED_PERFORMANCE");
-            } else {
-                mHintManager->EndHint("SUSTAINED_PERFORMANCE");
             }
-            mSustainedPerfModeOn = enabled;
+            mSustainedPerfModeOn = true;
             break;
         case Mode::LAUNCH:
+            if (mSustainedPerfModeOn) {
+                break;
+            }
             [[fallthrough]];
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
@@ -136,9 +122,6 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         case Mode::AUDIO_STREAMING_LOW_LATENCY:
             [[fallthrough]];
         default:
-            if (mBatterySaverOn || mSustainedPerfModeOn) {
-                break;
-            }
             if (enabled) {
                 mHintManager->DoHint(toString(type));
             } else {
@@ -166,7 +149,7 @@ ndk::ScopedAStatus Power::setBoost(Boost type, int32_t durationMs) {
     ATRACE_INT(toString(type).c_str(), durationMs);
     switch (type) {
         case Boost::INTERACTION:
-            if (mSustainedPerfModeOn || mBatterySaverOn) {
+            if (mSustainedPerfModeOn) {
                 break;
             }
             mInteractionHandler->Acquire(durationMs);
@@ -178,7 +161,7 @@ ndk::ScopedAStatus Power::setBoost(Boost type, int32_t durationMs) {
         case Boost::AUDIO_LAUNCH:
             [[fallthrough]];
         default:
-            if (mSustainedPerfModeOn || mBatterySaverOn) {
+            if (mSustainedPerfModeOn) {
                 break;
             }
             if (durationMs > 0) {
