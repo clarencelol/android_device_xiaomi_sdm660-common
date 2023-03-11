e1f4c4a6ca2d4b9733ca5faed554699580ba2c17
clarencelol <clarencekuiek@icloud.com>
2023-03-11 22:07:02 +0800

    sdm660-common: Move bluetooth profiles prop to product
    * Add few lines along the way
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -174,6 +174,7 @@ TARGET_PER_MGR_ENABLED := true
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_ODM_PROP += $(COMMON_PATH)/properties/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/properties/product.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/properties/system_ext.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor.prop
