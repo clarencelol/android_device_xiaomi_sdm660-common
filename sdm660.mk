71017d6b024ab00c7e46b7d91d0498b92bd89701
clarencelol <clarencekuiek@proton.me>
2024-02-20 18:15:46 +0800

    sdm660-common: Nuked VNDK 28
    A14 Chad
    error: VNDK version 28 not found.
    
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -566,7 +566,7 @@ PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
 
 # VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
